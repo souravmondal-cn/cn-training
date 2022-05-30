0249a2ec1455baea2476e2996e03d0b17bfd6fd1
souravm <souravm@capitalnumbers.com>
2022-05-30 13:50:01 +0530

    file deleted

--- functions.php
+++ functions.php
@@ -17,9 +17,6 @@ if ( ! function_exists( 'twentytwentytwo_support' ) ) :
 	 */
 	function twentytwentytwo_support() {
 
-		// Add support for block styles.
-		add_theme_support( 'wp-block-styles' );
-
 		// Enqueue editor styles.
 		add_editor_style( 'style.css' );
 

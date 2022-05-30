b882a7f8ff299100fa95236c5ff88821ca4ca6ef
souravm <souravm@capitalnumbers.com>
2022-05-30 13:32:45 +0530

    modified

--- functions.php
+++ functions.php
@@ -1,11 +1,6 @@
 <?php
 /**
  * Twenty Twenty-Two functions and definitions
- *
- * @link https://developer.wordpress.org/themes/basics/theme-functions/
- *
- * @package WordPress
- * @subpackage Twenty_Twenty_Two
  * @since Twenty Twenty-Two 1.2
  */
 
